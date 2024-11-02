0099b7786ac4e88e4e037bc40a0b21dce20b8c40
LuzLee111 <maryluz2607133@gmail.com>
2024-11-02 10:56:50 -0300

    Add files via upload

---
+++ src/main/java/org/example/controllers/StatsController.java
@@ -0,0 +1,28 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.example.services.StatsService;
+import org.example.dto.StatsResponse;
+
+@RestController
+@RequestMapping("/stats")
+public class StatsController {
+    private final StatsService statsService;
+
+    public StatsController(StatsService statsService) {
+        this.statsService = statsService;
+    }
+
+    @GetMapping
+    public StatsResponse getStats() {
+        return statsService.getStats();
+    }
+
+
+}
+
+
+
+
